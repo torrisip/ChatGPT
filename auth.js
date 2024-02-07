7963f47d59587f09b04ea3a4d6001e8969ae3648
torrisip <159198683+torrisip@users.noreply.github.com>
2024-02-07 23:40:42 +0100

    Update config auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,3 @@
-// Initialize Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyDI2NuEU1YmV4bRlA4apifmxN9jqSg3lkc",
-    authDomain: "chatgpt-6cdee.firebaseapp.com",
-    projectId: "chatgpt-6cdee",
-    storageBucket: "chatgpt-6cdee",
-//    messagingSenderId: "SENDER_ID",
-//    appId: "APP_ID"
-};
-
-firebase.initializeApp(firebaseConfig);
-
 // Login form
 const loginForm = document.getElementById('loginForm');
 
