6c8e1527944019247f3b12d671033fcbca803851
torrisip <159198683+torrisip@users.noreply.github.com>
2024-02-07 22:40:13 +0100

    Update and rename Auth.js to auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
+    apiKey: "AIzaSyDI2NuEU1YmV4bRlA4apifmxN9jqSg3lkc",
+    authDomain: "chatgpt-6cdee.firebaseapp.com",
+    projectId: "chatgpt-6cdee",
+    storageBucket: "chatgpt-6cdee",
+//    messagingSenderId: "SENDER_ID",
+//    appId: "APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
