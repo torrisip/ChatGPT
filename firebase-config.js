e58f135679736cffa2abae45ad3e5fd9b61b6de3
torrisip <159198683+torrisip@users.noreply.github.com>
2024-02-07 23:51:28 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDI2NuEU1YmV4bRlA4apifmxN9jqSg3lkc",
     authDomain: "chatgpt-6cdee.firebaseapp.com",
     projectId: "chatgpt-6cdee",
-    storageBucket: "chatgpt-6cdee",
+    storageBucket: "chatgpt-6cdee.appspot.com",
 //    messagingSenderId: "SENDER_ID",
 //    appId: "APP_ID"
 };
