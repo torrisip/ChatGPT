39cdb9489666caa9d04072067ebb047420bde8c5
torrisip <159198683+torrisip@users.noreply.github.com>
2024-02-07 23:33:05 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+
+const firebaseConfig = {
+    apiKey: "LA_TUA_API_KEY",
+    authDomain: "IL_TUO_PROGETTO.firebaseapp.com",
+    projectId: "IL_TUO_PROGETTO",
+    storageBucket: "IL_TUO_PROGETTO.appspot.com",
+    messagingSenderId: "IL_TUO_SENDER_ID",
+    appId: "IL_TUO_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
