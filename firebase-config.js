4c3a26a0ea3e344aea4634fe23401cace767e39b
p.torrisi <p.torrisi@reply.it>
2024-02-22 18:06:36 +0100

    prova

--- firebase-config.js
+++ firebase-config.js
@@ -5,8 +5,8 @@ const firebaseConfig = {
     apiKey: "AIzaSyDI2NuEU1YmV4bRlA4apifmxN9jqSg3lkc",
     authDomain: "chatgpt-6cdee.firebaseapp.com",
     projectId: "chatgpt-6cdee",
-    storageBucket: "chatgpt-6cdee.appspot.com"
-//    messagingSenderId: "SENDER_ID",
+    storageBucket: "chatgpt-6cdee.appspot.com",
+    messagingSenderId: "443840321392"
 //    appId: "APP_ID"
 };
 
